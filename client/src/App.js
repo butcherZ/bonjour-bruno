62f53f25776c5340bf8e05f2bb4d3e4e842a005f
Ziyi <ziyi@coding-days.com>
2022-04-30 07:00:07 +0200

    feat(): add style and button for updating isValid

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,63 @@
 import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
-
+import "./style/style.css";
 function App() {
   const [data, setData] = useState(null);
+  const [isSending, setIsSending] = useState(false);
   const fetchData = useCallback(async () => {
-    axios
-      .get(`http://localhost:3001/`)
-      .then((response) => setData(response.data));
+    const response = await axios.get(`http://localhost:3001/get`);
+    setData(response.data);
   }, []);
+
+  const sendRequest = useCallback(async () => {
+    if (isSending) return;
+    // update state
+    setIsSending(true);
+    // send the actual request
+    await fetch("http://localhost:3001/post", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    // once the request is sent, update state again
+    setIsSending(false);
+  }, [isSending, data]);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
 
-  console.log(data);
+  console.log("data is", data);
   return (
     <div className="App">
-      <header className="App-header">
-        <p>{data}</p>
+      <header className="header">
+        <h1>
+          <span>Bonjour </span>
+          <span>Bruno</span>
+        </h1>
       </header>
+      <body>
+        <main>
+          <p>
+            Chaque jour, une nouvelle photo de notre petillant ministre de
+            l'economie et des finances !
+          </p>
+        </main>
+        {data && data.isValid && (
+          <div className="gallery">
+            <img src={data.url} alt="Bruno Le Maire"></img>
+          </div>
+        )}
+        <button className="ghost" disabled={isSending} onClick={sendRequest}>
+          This is not Bruno!
+        </button>
+      </body>
+      <footer>
+        <p>Made From Paris with ♥</p>
+      </footer>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,17 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const job = require("./schedule");
-
+const Resource = require("./models/resource");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3001;
-const keyword = "Bruno Le Maire";
-const start = 23;
-const URL = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.SEARCH_ENGINE_ID}&q=${keyword}&searchType=image&fileType=png&imageSize=LARGE&gl=fr&imgType=face&dataRestrict=d[10]&start=${start}`;
 
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
@@ -25,19 +29,36 @@ app.get("/", (req, res) => {
   res.send("hello from server");
 });
 
+app.get("/get", (req, res) => {
+  // Get the count of all resources
+  Resource.count().exec(function (err, count) {
+    // Get a random entry
+    var random = Math.floor(Math.random() * count);
+
+    // Again query all resources but only fetch one offset by random
+    Resource.findOne()
+      .skip(random)
+      .exec(function (err, result) {
+        res.send(result);
+      });
+  });
+});
+
+app.post("/post", (req, res) => {
+  const data = req.body;
+  Resource.findByIdAndUpdate(
+    data._id,
+    { isValid: false },
+    function (err, docs) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Updated Resource : ", docs);
+      }
+    }
+  );
+});
+job();
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-job();
-//getResource(12);
-// axios
-//   .get(URL)
-//   .then((response) => {
-//     const result = response.data.items;
-//     const links = result.map((x) => x.link);
-//     console.log("------", links);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
